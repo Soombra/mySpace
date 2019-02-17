c1e491f2dba87ea5eed74a3df41dd2e08200e5a3
武二康 <bunikou@wuerkangdeMBP.lan>
2019-02-17 22:22:49 +0800

    123

--- server/index.js
+++ server/index.js
@@ -36,7 +36,7 @@ async function start() {
   app.use(nuxt.render)
 
   // Listen the server
-  app.listen(port, host)
+  app.listen(port)
   consola.ready({
     message: `Server listening on http://${host}:${port}`,
     badge: true
